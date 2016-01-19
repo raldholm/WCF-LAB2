f762bb7f7536f2aeb70a378166d0b4a7dfffa20a
Martin Raldholm <martin@raldholm.se>
2016-01-19 10:41:10 +0100

    Finished BMIService

--- BMIService/Program.cs
+++ BMIService/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
@@ -15,10 +16,41 @@ namespace BMIService
         [OperationContract]
         double CalculateBmi(double weight, double length);
     }
+    public class BmiCalculatorService : IBmiCalculatorService
+    {
+        public double CalculateBmi(double weight, double length)
+        {
+            var lengtInMeter = length / 100;
+            return weight / (lengtInMeter * lengtInMeter);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var baseAdress = new Uri("http://localhost:8082/BMIService");
+            var selfService = new ServiceHost(typeof(BmiCalculatorService), baseAdress);
+            try
+            {
+                selfService.AddServiceEndpoint(typeof (IBmiCalculatorService), new WSHttpBinding(), "BMIService");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                {
+                    smb.HttpGetEnabled = true;
+                }
+                selfService.Description.Behaviors.Add(smb);
+                selfService.Open();
+                Console.WriteLine("Tjänsten är startad!");
+                Console.WriteLine("Tryck på enter för att avsluta.");
+                Console.ReadLine();
+
+            }
+            catch (CommunicationException ex)
+            {
+                Console.WriteLine("Kommunikationsfel: " +ex.Message);
+                selfService.Abort();
+                Console.ReadKey();
+            }
+
         }
     }
 }

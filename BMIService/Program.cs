eac52fc170c03021533b58a880541c4f0f05ec87
Martin Raldholm <martin@raldholm.se>
2016-01-19 10:24:47 +0100

    Added IBmiCalculatorService

--- BMIService/Program.cs
+++ BMIService/Program.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 
 namespace BMIService
 {
+    [ServiceContract(Namespace = "http://BMIService")]
+
+    public interface IBmiCalculatorService
+    {
+        [OperationContract]
+        double CalculateBmi(double weight, double length);
+    }
     class Program
     {
         static void Main(string[] args)

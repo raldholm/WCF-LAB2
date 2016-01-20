3c70c2104f990a6f2ddbdd37ea9df20bca663f89
Martin Raldholm <martin@raldholm.se>
2016-01-20 19:40:50 +0100

    Fixed ageservice and added client.

--- AgeClients/AgeClients/Program.cs
+++ AgeClients/AgeClients/Program.cs
@@ -10,6 +10,13 @@ namespace AgeClients
     {
         static void Main(string[] args)
         {
+            var client = new AgeCalculatorServiceClient();
+            Console.WriteLine("Mata in ditt födelsedatum. (YYYY-MM-DD)");
+            var birthdate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Du är {0} dagar gammal.", client.CalculateAgedays(birthdate));
+            Console.WriteLine("Du fyller jämt antal tusen dagar på datumet: {0}",client.CalculateEvenBirthday(birthdate) );
+            Console.ReadKey();
         }
     }
 }

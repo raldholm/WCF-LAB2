ce3cf179a292cb259f6563c7dd60988ce1a5c6e6
Martin Raldholm <martin@raldholm.se>
2016-01-20 21:58:37 +0100

    added CalculateNikotine Method

--- SnusService/SnusService/Program.cs
+++ SnusService/SnusService/Program.cs
@@ -8,6 +8,24 @@ using System.ServiceModel;
 
 namespace SnusService
 {
+    [ServiceContract(Namespace = "http://SnusService")]
+
+    public interface ISnusService
+    {
+        [OperationContract]
+        double CalculateNicotineAmount(double antalDosor);
+    }
+
+    public class SnusCalculator : ISnusService
+    {
+        public double CalculateNicotineAmount(double antalDosor)
+        {
+            var gramNikoin = antalDosor * (16 * 0.008);
+            var milligramNikotin = gramNikoin/100;
+            return milligramNikotin;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
